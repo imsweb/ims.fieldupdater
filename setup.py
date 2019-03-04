e72ffefc393c54485ab4582d9e0cc1c23de74171
wohnlice <wohnlice@imsweb.com>
2019-03-04 13:10:40 -0500

    py3 and cleanup

--- setup.py
+++ setup.py
@@ -23,6 +23,8 @@ setup(name='ims.fieldupdater',
           "Framework :: Plone :: 5.0",
           "Framework :: Plone :: 5.1",
           "Programming Language :: Python",
+          "Programming Language :: Python :: 2.7",
+          "Programming Language :: Python :: 3"
       ],
       author='Eric Wohnlich',
       author_email='wohnlice@imsweb.com',
